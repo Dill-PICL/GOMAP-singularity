7c417aa52de03718905bb19931cb785683918013
Gokul Wimal <kokul@iastate.edu>
2018-06-28 11:33:03 -0500

    changes to setup

--- setup-GOMAP.sh
+++ setup-GOMAP.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+if [ ! -f "$HOME/.irods/irods_environment.json" ]
+then
+	echo "File not found"
+	mkdir -p $HOME/.irods/ && cp irods_environment.json $HOME/.irods/
+else
+	echo "Using $HOME/.irods/irods_environment.json for icommands"
+fi
+
 instance_name="GOMAP"
 img_loc="$instance_name.simg"
 mkdir -p $PWD/tmp $PWD/GOMAP-data
