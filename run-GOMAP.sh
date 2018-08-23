550633b07c084373e90b243ffa0cf700bd7c6262
Gokul Wimal <kokul@iastate.edu>
2018-08-23 09:40:02 -0500

    removed dependencies

--- run-GOMAP.sh
+++ run-GOMAP.sh
@@ -2,4 +2,4 @@
 
 export GOMAP_LOC="$PWD/GOMAP.simg" && export GOMAP_DATA_LOC="$PWD/GOMAP-data"
 
-./run-GOMAP.py --config test/config.yml --tmpdir=$HOME/tmpdir --step=$1 --dev=GOMAP
+./run-GOMAP.py --config test/config.yml --tmpdir=$HOME/tmpdir --step=$1
