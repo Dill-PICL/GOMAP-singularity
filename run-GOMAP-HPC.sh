2eb575546dd292aa4083112d6090b4050672ab13
Gokul Wimal <kokul@iastate.edu>
2018-08-28 13:37:35 -0500

    adding nodes needed for job submission

--- run-GOMAP-HPC.sh
+++ run-GOMAP-HPC.sh
@@ -11,7 +11,8 @@ fi
 if [ ! -f "$GOMAP_LOC" ]
 then
     module load singularity/2.6.0 && \
-    singularity pull --name "$GOMAP_LOC" shub://Dill-PICL/GOMAP-singularity:bridges
+    SINGULARITY_PULLFOLDER=`dirname $GOMAP_LOC`
+    singularity pull --name `basename $GOMAP_LOC` shub://Dill-PICL/GOMAP-singularity:bridges
 fi
 
 config=$1
