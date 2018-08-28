9709de691460ca97aef0e9f1b79dba5b445d7980
Gokul Wimal <kokul@iastate.edu>
2018-08-28 12:41:58 -0500

    removing unnecessary scripts

--- run-GOMAP.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export GOMAP_LOC="$PWD/GOMAP.simg" && export GOMAP_DATA_LOC="$PWD/GOMAP-data"
-
-mpirun -n 2 -host master,slave ./run-GOMAP.py --config test/config.yml --tmpdir=$HOME/tmpdir --step=$1
