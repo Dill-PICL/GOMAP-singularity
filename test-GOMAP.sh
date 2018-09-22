1ad119ecb3622299741df48daf4be86c6e287706
Kokulapalan Wimalanathan <kokulapalan@gmail.com>
2018-09-22 12:32:41 +0100

    adding test files to test the pipeline after downloading container

---
+++ test-GOMAP.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+instance_name="GOMAP"
+img_loc="$instance_name.simg"
+mkdir -p $PWD/tmp
+
+if [ ! -f "$img_loc" ]
+then
+	singularity pull --name "$img_loc" shub://Dill-PICL/GOMAP-singularity:single
+fi
+
+./stop-GOMAP.sh
+
+singularity instance.start \
+	--bind $PWD/GOMAP-data/mysql/lib:/var/lib/mysql \
+	--bind $PWD/GOMAP-data/mysql/log:/var/log/mysql \
+	--bind $PWD/GOMAP-data:/opt/GOMAP/data \
+    --bind $PWD:/workdir \
+	-W $PWD/tmp \
+	$img_loc $instance_name && \
+singularity run instance://$instance_name --step=seqsim --config=test/config.yml && \
+singularity run instance://$instance_name --step=domain --config=test/config.yml && \
+singularity run instance://$instance_name --step=mixmeth-blast --config=test/config.yml && \
+singularity run instance://$instance_name --step=mixmeth-preproc --config=test/config.yml && \
+singularity run instance://$instance_name --step=mixmeth --config=test/config.yml && \
+singularity run instance://$instance_name --step=aggregate --config=test/config.yml
+
+
+
+./stop-GOMAP.sh
