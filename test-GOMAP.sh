5233aac863b3ac443587a45382897f54a6195bd6
Wimalanathan Kokulapalan - student <kokul@condo2017.its.iastate.edu>
2018-09-25 05:18:34 -0500

    added tmpdir bind to test script

--- test-GOMAP.sh
+++ test-GOMAP.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 instance_name="GOMAP"
-img_loc="$instance_name.simg"
+img_loc="$PWD/$instance_name.simg"
 mkdir -p $PWD/tmp
 
 if [ ! -f "$img_loc" ]
 then
-	singularity pull --name "$img_loc" shub://Dill-PICL/GOMAP-singularity:single
+	singularity pull --name "$img_loc" shub://Dill-PICL/GOMAP-singularity
 fi
 
 ./stop-GOMAP.sh
@@ -14,9 +14,11 @@ singularity instance.start \
 	--bind $PWD/GOMAP-data/mysql/lib:/var/lib/mysql \
 	--bind $PWD/GOMAP-data/mysql/log:/var/log/mysql \
 	--bind $PWD/GOMAP-data:/opt/GOMAP/data \
-    --bind $PWD:/workdir \
+	--bind $PWD:/workdir \
+	--bind $TMPDIR:/tmpdir \
 	-W $PWD/tmp \
 	$img_loc $instance_name && \
+sleep 10 && \
 singularity run instance://$instance_name --step=seqsim --config=test/config.yml && \
 singularity run instance://$instance_name --step=domain --config=test/config.yml && \
 singularity run instance://$instance_name --step=mixmeth-blast --config=test/config.yml && \
