18d5505458a1badfd7507fa0fc0a2dd29087cec9
Kokulapalan Wimalanathan <kokulapalan@gmail.com>
2018-10-05 16:12:59 -0500

    fixing issues

--- run-GOMAP-condo.sh
+++ run-GOMAP-condo.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]
-then
-    echo "Please check the number of input arguments"
-    echo "run-SINGLE.sh config_file step number_of_nodes"
-    echo "run-SINGLE.sh test/config.yml seqsim 1"
-    exit
-fi
+GOMAP_LOC="$PWD/GOMAP.simg"
+GOMAP_DATA_LOC="$PWD/GOMAP-data"
 
 if [ ! -f "$GOMAP_LOC" ]
 then
@@ -15,51 +10,38 @@ then
     singularity pull --name `basename $GOMAP_LOC` shub://Dill-PICL/GOMAP-singularity:bridges
 fi
 
-config=$1
-step=$2
-nodes=$3
-name=`cat $config | grep -v "#" | fgrep basename | cut -f 2 -d ":" | tr -d ' '`
-tmpdir="\$RAMDISK"
-echo -e "#!/bin/bash
-#SBATCH -N $nodes
-#SBATCH --ntasks-per-node 1
-#SBATCH -t 48:00:00
-#SBATCH --job-name=$name-GOMAP-$step
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=kokul@iastate.edu
-#SBATCH -o %x.out
-#SBATCH -e %x.err
-#SBATCH -C EGRESS
+args="$@"
 
-
-module load mpi/gcc_mvapich  singularity/2.6.0
-
-" > "$name-GOMAP-$step.job"
-
-if [ "$step" == "mixmeth" ]
+if [[ "$SLURM_CLUSTER_NAME" = "condo2017" ]]
 then
-echo "
-singularity instance.start   \\
-    --bind $GOMAP_DATA_LOC/mysql/lib:/var/lib/mysql  \\
-    --bind $GOMAP_DATA_LOC/mysql/log:/var/log/mysql  \\
-    --bind $GOMAP_DATA_LOC:/opt/GOMAP/data \\
-    --bind $PWD:/workdir  \\
-    --bind $tmpdir:/tmpdir  \\
-    -W $PWD/tmp \\
-    $GOMAP_LOC GOMAP && \\
-singularity run \\
-    instance://GOMAP --step=$step --config=$config" >> "$name-GOMAP-$step.job"
+    tmpdir="$TMPDIR"
 else
-echo "
-mpiexec -n $nodes \\
-singularity run   \\
-    --bind $GOMAP_DATA_LOC/mysql/lib:/var/lib/mysql  \\
-    --bind $GOMAP_DATA_LOC/mysql/log:/var/log/mysql  \\
-    --bind $GOMAP_DATA_LOC:/opt/GOMAP/data \\
-    --bind $PWD:/workdir  \\
-    --bind $tmpdir:/tmpdir  \\
-    -W $PWD/tmp \\
-    $GOMAP_LOC --step=$step --config=$config" >> "$name-GOMAP-$step.job"
+    tmpdir="/tmp"
 fi
 
-sbatch  "$name-GOMAP-$step.job"
+if [[ "$args" = *"mixmeth"* ]]
+then
+    echo "Starting GOMAP instance"
+    echo singularity instance.start   \
+        --bind $GOMAP_DATA_LOC/mysql/lib:/var/lib/mysql  \
+        --bind $GOMAP_DATA_LOC/mysql/log:/var/log/mysql  \
+        --bind $GOMAP_DATA_LOC:/opt/GOMAP/data \
+        --bind $PWD:/workdir  \
+        --bind $tmpdir:/tmpdir  \
+        -W $PWD/tmp \
+        $GOMAP_LOC GOMAP && \
+        sleep 10 && \
+    singularity run \
+        instance://GOMAP $@
+    ./stop-GOMAP.sh
+else
+    echo "Running GOMAP $@"
+    echo mpiexec -n $SLURM_JOB_NUM_NODES singularity run   \
+        --bind $GOMAP_DATA_LOC/mysql/lib:/var/lib/mysql  \
+        --bind $GOMAP_DATA_LOC/mysql/log:/var/log/mysql  \
+        --bind $GOMAP_DATA_LOC:/opt/GOMAP/data \
+        --bind $PWD:/workdir  \
+        --bind $tmpdir:/tmpdir  \
+        -W $PWD/tmp \
+        $GOMAP_LOC $@
+fi
