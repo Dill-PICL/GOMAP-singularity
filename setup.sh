d843b0e3a822d29e8adf94a787b4e67745831f0d
Kokulapalan Wimalanathan <kokul@iastate.edu>
2019-09-23 17:07:03 -0500

    added s3 URL for downloading the image

--- setup.sh
+++ setup.sh
@@ -14,12 +14,12 @@ fi
 
 # Declaring variables for different options
 
-export ZENODO_URL="https://sandbox.zenodo.org/record/274851/files/GOMAP.simg?download=1"
+export IMG_URL="http://gomap-data.s3-website.us-east-2.amazonaws.com/GOMAP-1.3/imgs/GOMAP.simg"
 export GOMAP_IMG="GOMAP.simg"
 
 if [ ! -f $GOMAP_LOC/$GOMAP_IMG ]
 then
-    wget $ZENODO_URL -O $GOMAP_LOC/$GOMAP_IMG
+    wget $IMG_URL -O $GOMAP_LOC/$GOMAP_IMG
 else
     echo "The $GOMAP_LOC/$GOMAP_IMG exists" > /dev/stderr
     echo "Delete the image if you want to download it" /dev/stderr
