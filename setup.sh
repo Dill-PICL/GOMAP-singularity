7291b226a95b4aa59d94b236ea8bb23c0816ce29
Wimalanathan Kokulapalan <wkpalan@users.noreply.github.com>
2020-11-08 23:27:43 -0500

    adding the script changes to make testing faster

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ export IRODS_USER_NAME="anonymous"
 export IRODS_ZONE_NAME="iplant"
 export ICOMMANDS_IMG="icommands.sif"
 export GOMAP_IMG="GOMAP.sif"
-export GOMAP_URL="/iplant/home/shared/dillpicl/gomap/GOMAP/1.3.2/$GOMAP_IMG"
+export GOMAP_URL="/iplant/home/shared/dillpicl/gomap/GOMAP/v1.3.3/$GOMAP_IMG"
 
 if [ ! -f $GOMAP_LOC/$GOMAP_IMG ]
 then
@@ -24,5 +24,5 @@ then
     rm $ICOMMANDS_IMG
 else
     echo "The $GOMAP_LOC/$GOMAP_IMG exists" > /dev/stderr
-    echo "Delete the image if you want to download it" /dev/stderr
+    echo "Delete the image if you want to download it again" > /dev/stderr
 fi
