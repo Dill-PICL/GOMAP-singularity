f756cdec7df29151093e539bb0ff2ff948d7c4d5
Ubuntu <wkpalan@users.noreply.github.com>
2020-11-26 16:59:20 +0000

    adding the updated lines to download images

--- setup.sh
+++ setup.sh
@@ -11,13 +11,17 @@ fi
 
 export ICOMMANDS_IMG="icommands.sif"
 export GOMAP_IMG="GOMAP.sif"
-export GOMAP_URL="/iplant/home/shared/dillpicl/gomap/GOMAP/1.3.3/$GOMAP_IMG"
+export GOMAP_URL="/iplant/home/shared/dillpicl/gomap/GOMAP/v1.3.3/$GOMAP_IMG"
+export SINGULARITY_BINDPATH="$GOMAP_LOC:$GOMAP_LOC"
 
-if [ ! -f $GOMAP_LOC/$GOMAP_IMG ]
+
+if [ ! -f $ICOMMANDS_IMG ]
 then
-    singularity pull -F $ICOMMANDS_IMG shub://wkpalan/icommands-cyverse:latest && \
-    singularity run $ICOMMANDS_IMG irsync -v i:$GOMAP_URL $GOMAP_LOC/$GOMAP_IMG && \
-    rm $ICOMMANDS_IMG
+    singularity pull -F $ICOMMANDS_IMG shub://wkpalan/icommands-cyverse:latest
+elif [ ! -f $GOMAP_LOC/$GOMAP_IMG ]
+then
+    singularity run $ICOMMANDS_IMG ls $GOMAP_LOC
+    singularity run $ICOMMANDS_IMG iget -P $GOMAP_URL $GOMAP_LOC/$GOMAP_IMG
 else
     echo "The $GOMAP_LOC/$GOMAP_IMG exists" > /dev/stderr
     echo "Delete the image if you want to download it" /dev/stderr
