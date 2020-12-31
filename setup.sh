f96e24b3100b8acf61abc3a4fe5716d4b434c189
Wimalanathan Kokulapalan <wkpalan@users.noreply.github.com>
2020-12-30 23:09:09 -0500

    adding the irods config file and additional steps for downloading the GOMAP image.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ then
 fi
 
 # Declaring variables for different options
-
 export IRODS_HOST="data.cyverse.org"
 export IRODS_PORT="1247"
 export IRODS_USER_NAME="anonymous"
@@ -18,9 +17,21 @@ export GOMAP_IMG="GOMAP.sif"
 export GOMAP_VERSION="v1.3.4"
 export GOMAP_URL="/iplant/home/shared/dillpicl/gomap/GOMAP/$GOMAP_VERSION/$GOMAP_IMG"
 
+
 if [ ! -f $GOMAP_LOC/$GOMAP_IMG ]
 then
-    singularity pull -F $ICOMMANDS_IMG shub://wkpalan/icommands-cyverse:latest && \
+    # Making the irods directory and copying files if they don't exist    
+    if [ ! -f "~/.irods/irods_environmnt.json" ]
+    then
+        mkdir -p ~/.irods &&
+        cp irods_environment.json ~/.irods/
+    fi
+    #Downloading icommands image if it doesn't exist.
+    if [ ! -f $ICOMMANDS_IMG ]
+    then
+        singularity pull -F $ICOMMANDS_IMG shub://wkpalan/icommands-cyverse:latest
+    fi
+    #Downloading GOMAP Image
     singularity run $ICOMMANDS_IMG iget -PvT $GOMAP_URL $GOMAP_LOC/$GOMAP_IMG && \
     rm $ICOMMANDS_IMG
 else
